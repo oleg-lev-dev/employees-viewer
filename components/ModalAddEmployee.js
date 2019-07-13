a63aabcf7d2e0aede0818b4abac6f47b2f61f217
Oleg Levchenko <oleg.levchenko.dev@gmail.com>
2019-07-13 19:18:41 +0300

    Refactor Employees Route to functional component

--- components/ModalAddEmployee.js
+++ components/ModalAddEmployee.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-import Form from 'react-bootstrap/Form';
-import {useField, useForm} from 'react-final-form-hooks';
+import {useForm} from 'react-final-form-hooks';
 import PropTypes from 'prop-types';
 
 import {TEmployee} from '../helpers/types';
+import FormGroup from '../components/FormGroup';
 
 const emptyEmployee = {
   firstName: '',
@@ -14,19 +14,17 @@ const emptyEmployee = {
   description: '',
 };
 
+const validate = (value) => {
+  return value && value.length > 0 ? undefined : 'Обязательное поле'
+};
+
 function ModalAddEmployee({onSave, onHide, show, employee}) {
-  const {form, handleSubmit, values, pristine, submitting} = useForm({
+  const {form, handleSubmit, pristine, submitting, invalid} = useForm({
     onSubmit: onSave,
-    initialValues: employee || emptyEmployee
-    //validate // a record-level validation function to check all form values
+    initialValues: employee || emptyEmployee,
   });
   const isEditing = !!employee;
   const title = isEditing ? `Edit "${employee.firstName}"` : 'Add New Employee';
-
-  const firstName = useField('firstName', form);
-  const lastName = useField('lastName', form);
-  const position = useField('position', form);
-  const description = useField('description', form);
 
   return (
     <Modal show={show}
@@ -38,38 +36,31 @@ function ModalAddEmployee({onSave, onHide, show, employee}) {
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <Form.Group controlId="formGroupFirstName">
-          <Form.Label>First Name</Form.Label>
-          <Form.Control type="text"
-                        placeholder="Enter First Name"
-                        {...firstName.input}/>
-        </Form.Group>
-
-        <Form.Group controlId="formGroupLastName">
-          <Form.Label>Last Name</Form.Label>
-          <Form.Control type="text"
-                        placeholder="Enter Last Name"
-                        {...lastName.input}/>
-        </Form.Group>
-
-        <Form.Group controlId="formGroupPosition">
-          <Form.Label>Position</Form.Label>
-          <Form.Control type="text"
-                        placeholder="Enter Position"
-                        {...position.input}/>
-        </Form.Group>
-
-        <Form.Group controlId="formGroupDescription">
-          <Form.Label>Description</Form.Label>
-          <Form.Control as="textarea"
-                        rows="3"
-                        placeholder="Enter Description"
-                        {...description.input}/>
-        </Form.Group>
+        <FormGroup field="firstName"
+                   form={form}
+                   placeholder="Enter First Name"
+                   label="First Name"
+                   validate={validate}/>
+        <FormGroup field="lastName"
+                   form={form}
+                   placeholder="Enter Last Name"
+                   label="Last Name"
+                   validate={validate}/>
+        <FormGroup field="position"
+                   form={form}
+                   placeholder="Enter Position"
+                   label="Position"
+                   validate={validate}/>
+        <FormGroup field="description"
+                   form={form}
+                   as="textarea"
+                   rows="3"
+                   placeholder="Enter Description"
+                   label="Description"/>
       </Modal.Body>
 
       <Modal.Footer>
-        <Button disabled={pristine || submitting} onClick={handleSubmit}>Save</Button>
+        <Button disabled={pristine || submitting || invalid} onClick={handleSubmit}>Save</Button>
       </Modal.Footer>
     </Modal>
   )
